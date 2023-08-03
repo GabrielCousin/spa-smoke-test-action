f8d77f68742b314fe23e8d385879cda6fb0aa3c5
Gabriel Cousin <7649529+GabrielCousin@users.noreply.github.com>
2023-08-03 21:00:45 +0200

    fix(action): deprecations

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import core from "@actions/core";
+import { getInput, setFailed } from "@actions/core";
 import puppeteer from "puppeteer";
 import { wait } from "./utils/wait";
 
 async function run(): Promise<void> {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: "new",
+  });
 
-  const waitMsInput = core.getInput("wait-on-start");
+  const waitMsInput = getInput("wait-on-start");
   const waitMs = waitMsInput ? parseInt(waitMsInput) : 0;
 
-  const url = core.getInput("target-url", { required: true });
-  const selector = core.getInput("target-selector", { required: true });
+  const url = getInput("target-url", { required: true });
+  const selector = getInput("target-selector", { required: true });
 
   await wait(waitMs);
 
@@ -20,7 +22,7 @@ async function run(): Promise<void> {
     console.log("Smoke test succeeded");
   } catch (error) {
     console.log(error);
-    core.setFailed("Smoke test failed");
+    setFailed("Smoke test failed");
   }
 
   await browser.close();
