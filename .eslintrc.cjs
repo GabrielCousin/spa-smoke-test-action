6f498a18ce96da32a3dfac2743ab6100eab99d18
Gabriel Cousin <7649529+GabrielCousin@users.noreply.github.com>
2023-08-09 21:13:46 +0200

    chore(test): add jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,20 @@
-/* eslint-env node */
 module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
+  env: {
+    node: true,
+  },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
+  overrides: [
+    {
+      files: ["**/*.test.ts"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+    },
+  ],
 };
